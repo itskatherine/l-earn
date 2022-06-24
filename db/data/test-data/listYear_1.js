7cb7ddad9918c2abd17a3a8f2c473b58cd4955ff
mkpi-pm <mkpi@protonmail.comm>
2022-06-24 12:17:49 +0100

    add word lists files and json files

---
+++ db/data/test-data/listYear_1.js
@@ -0,0 +1,11 @@
+module.exports = [
+  { word: "the" },
+  { word: "we" },
+  { word: "pull" },
+  { word: "a" },
+  { word: "no" },
+  { word: "full" },
+  { word: "do" },
+  { word: "go" },
+  { word: "he" },
+];
---
+++ db/setup.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS l_earn_api;
+CREATE DATABASE l_earn_api;
+
+DROP DATABASE IF EXISTS l_earn_api_test;
+CREATE DATABASE l_earn_api_test;
